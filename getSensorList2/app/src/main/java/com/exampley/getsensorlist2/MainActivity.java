e107dffa99b633e2ad14b3547e522aafa49ecacc
yuri <dbfl4387@naver.com>
2021-04-06 20:39:59 +0900

    onClick Outside onCreate

--- getSensorList2/app/src/main/java/com/exampley/getsensorlist2/MainActivity.java
+++ getSensorList2/app/src/main/java/com/exampley/getsensorlist2/MainActivity.java
@@ -7,14 +7,17 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView mTxtSensors;
     SensorManager sensorMgr;
     List<Sensor> sensorList;
+    Button mretrieve_sensors_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +26,18 @@ public class MainActivity extends AppCompatActivity {
 
         mTxtSensors = findViewById(R.id.txtSensors);
         mTxtSensors.setMovementMethod(new ScrollingMovementMethod());
-
-        sensorMgr = (SensorManager)getSystemService(SENSOR_SERVICE);
-
+        mretrieve_sensors_btn = findViewById(R.id.retrieve_sensors_btn);
+        mretrieve_sensors_btn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
-        mTxtSensors.append("(# Sensor: " + sensorList.size()+")\n\n");
-        for(Sensor sensor : sensorList){
-            mTxtSensors.append("Sensor name :"+sensor.getName() + "\n");
-            mTxtSensors.append("Sensor type :"+sensor.getType() + "\n\n");
+        mTxtSensors.append("(# Sensor: " + sensorList.size() + ")\n\n");
+        for (Sensor sensor : sensorList) {
+            mTxtSensors.append("Sensor name :" + sensor.getName() + "\n");
+            mTxtSensors.append("Sensor type :" + sensor.getType() + "\n\n");
         }
-
 
     }
 }
