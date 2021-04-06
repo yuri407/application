4fcb65ea098159ab10bc6c51fe7f15e31a504fe3
yuri <dbfl4387@naver.com>
2021-04-06 10:05:11 +0900

    initial commit

---
+++ getSensorList/app/src/main/java/com/exampley/getsensorlist/MainActivity.java
@@ -0,0 +1,28 @@
+package com.exampley.getsensorlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTxtSensors;
+    SensorManager sensorMgr;
+    List<Sensor> sensorList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTxtSensors = findViewById(R.id.txtSensors);
+
+        sensorMgr = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
+    }
+}
