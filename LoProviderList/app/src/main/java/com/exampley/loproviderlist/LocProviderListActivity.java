249a6b2d22934a324133215e0a520655d032f4b2
yuri <dbfl4387@naver.com>
2021-04-07 15:17:42 +0900

    button to list loc. providers

--- LoProviderList/app/src/main/java/com/exampley/loproviderlist/LocProviderListActivity.java
+++ LoProviderList/app/src/main/java/com/exampley/loproviderlist/LocProviderListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -12,6 +14,7 @@ public class LocProviderListActivity extends AppCompatActivity {
     TextView mTextView;
     LocationManager locMgr;
     List<String> locProviders;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +23,19 @@ public class LocProviderListActivity extends AppCompatActivity {
 
         mTextView = findViewById(R.id.txtOutput);
         locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        button = findViewById(R.id.button);
 
         locProviders = locMgr.getAllProviders();
-
-        String s = "";
-        for(int i = 0; i < locProviders.size(); i++){
-            s += "Loc. Provider " + locProviders.get(i) + "\n"
-                    + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
-        }
-        mTextView.setText(s);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = "";
+                for (int i = 0; i < locProviders.size(); i++) {
+                    s += "Loc. Provider " + locProviders.get(i) + "\n"
+                            + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
+                }
+                mTextView.setText(s);
+            }
+        });
     }
 }
